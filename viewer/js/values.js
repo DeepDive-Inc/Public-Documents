5f14a3ce85953a46f5bcad17476bdc943a7508fe
はまち <31212444+HeyHamachi@users.noreply.github.com>
2024-12-05 23:35:06 +0900

    create viewer

---
+++ viewer/js/values.js
@@ -0,0 +1,11 @@
+Object.assign(
+  window,
+  {
+    values: {
+      AppName: "PublicDocumentViewer",
+      Organization: "DeepDive, Inc.",
+      Copyright: `© ${new Date().getFullYear()} DeepDive, Inc.`,
+      Version: `v0.1.0`,
+    }
+  }
+);
